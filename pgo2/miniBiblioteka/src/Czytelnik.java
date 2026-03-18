07e383f7c91244d8256147f50b1812413eaef46f
Hlib Seleznov <s30060@pjwstk.edu.pl>
2026-03-18 12:31:36 +0100

    test

---
+++ pgo2/miniBiblioteka/src/Czytelnik.java
@@ -0,0 +1,9 @@
+public class Czytelnik {
+    private String imie;
+    private String nazwisko;
+    private int numerKarty;
+    private int liczbaWypozyczen;
+    public Czytelnik(String imie,String nazwisko,int numerKarty, int liczbaWypozyczen){
+
+    }
+}
